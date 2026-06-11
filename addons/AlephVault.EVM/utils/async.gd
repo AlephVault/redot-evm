13f8e1d55838ed9a8bde2dc22f4fbc6012c728ce
Luis Masuelli <luisfmasuelli@gmail.com>
2026-06-10 20:30:25 -0500

    Adding another utility function in async.gd.

--- addons/AlephVault.EVM/utils/async.gd
+++ addons/AlephVault.EVM/utils/async.gd
@@ -1,4 +1,9 @@
 extends Object
 
-static func ok(value: Variant) -> Dictionary:
+## Creates a standard successful response.
+static func success(value: Variant) -> Dictionary:
 	return {"ok": true, "value": value}
+
+## Creates a standard failed response.
+static func failed(error: Variant) -> Dictionary:
+	return {"ok": false, "error": error}
