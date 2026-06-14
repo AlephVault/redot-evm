af953ad617aa2e3dee67e3d45101b040c78c8079
Luis Masuelli <luisfmasuelli@gmail.com>
2026-06-14 16:10:07 -0500

    Starting the sample hardhat project.

---
+++ addons/AlephVault.EVM/samples/blockchain/hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-common-tools");
+require("hardhat-enquirer-plus");
+require("hardhat-blueprints");
+require("hardhat-method-prompts");
+require("hardhat-ignition-deploy-everything");
+require("hardhat-openzeppelin-common-blueprints");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+};
